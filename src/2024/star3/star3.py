d1f64d311d08452b56a0606e0b4aa60abc030237
Kishan Thakurani <kishanthakurani2003@gmail.com>
2024-12-03 15:08:00 +0100

    Solved day 3 part 2

--- src/2024/star3/star3.py
+++ src/2024/star3/star3.py
@@ -15,11 +15,28 @@ def part1(filename):
 
 
 def part2(filename):
-    pass
+    line = ''.join(common(filename))
+    match = re.findall(r'mul\(\d{1,3},\d{1,3}\)|don\'t\(\)|do\(\)', line)
+    enabled = True
+    total = 0
+    for m in match:
+        if enabled:
+            if m == "don't()":
+                enabled = False
+                continue
+            if m == 'do()':
+                continue
+            m = m[4:-1].split(',')
+            total += int(m[0]) * int(m[1])
+        else:
+            if m == 'do()':
+                enabled = True
+
+    return total
 
 
 if __name__ == '__main__':
     assert part1('example.txt') == 161, f"Received {part1('example.txt')} for example on part1"
     print(f'Part 1 answer: {part1('input.txt')}')
-    assert part2('example.txt') == 48, f"Received {part2('example.txt')} for example on part2"
+    assert part2('example2.txt') == 48, f"Received {part2('example2.txt')} for example2 on part2"
     print(f'Part 2 answer: {part2('input.txt')}')
