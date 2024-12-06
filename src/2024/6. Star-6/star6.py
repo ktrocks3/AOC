c2dcb7d0b6aea03398d8ae7f20e6eb1d4cff3ab7
Kishan Thakurani <kishanthakurani2003@gmail.com>
2024-12-06 19:13:28 +0100

    Part 2 done D:
    Day 6 Part 2 took a while. Let's see how the rest goes...

--- src/2024/6. Star-6/star6.py
+++ src/2024/6. Star-6/star6.py
@@ -9,7 +9,6 @@ def common(filename):
 
 def part1(filename):
     grid = []
-    start = (0, 0)
     directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
     direction = directions[0]
     x, y = -1, -1
@@ -44,12 +43,57 @@ def part1(filename):
 
 
 def part2(filename):
-    pass
+    grid = []
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    start = ()
+    # Up, Right, Down, Left is the order
+
+    for line in common(filename):
+        row = []
+        for j, item in enumerate(line):
+            if item == '^':
+                start = (j, len(grid))
+                row.append(1)
+            elif item == '.':
+                row.append(0)
+            elif item == '#':
+                row.append(-1)
+            else:
+                print("Bad found at", len(grid), j, item)
+        grid.append(row)
+
+    def simulate(s, o):
+        x, y = s
+        g = [r[:] for r in grid]  # Deep copy of a 2D grid
+        g[o[1]][o[0]] = -1
+        d = directions[0]
+        while True:
+            dx, dy = x + d[0], y + d[1]
+            if dx < 0 or dx >= len(g[0]) or dy < 0 or dy >= len(g):
+                return False
+            if g[dy][dx] == -1:
+                d = directions[(directions.index(d) + 1) % len(directions)]
+                continue
+            x, y = dx, dy
+            if g[y][x] > 0 and g[y][x] & (1 << directions.index(d)):
+                return True
+            g[y][x] |= 1 << directions.index(d)
+        return True
+
+    count = 0
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            if (j, i) == start:
+                continue
+
+            if simulate(start, (i, j)):
+                count += 1
+
+    return count
 
 
 if __name__ == '__main__':
     assert part1('example.txt') == 41, f"Received {part1('example.txt')} for example on part1"
     print(f'Part 1 answer: {part1('input.txt')}')
-    assert part2('example.txt') == "", f"Received {part2('example.txt')} for example on part2"
-    assert part2('example2.txt') == "", f"Received {part2('example2.txt')} for example2 on part2"
+    assert part2('example.txt') == 6, f"Received {part2('example.txt')} for example on part2"
     print(f'Part 2 answer: {part2('input.txt')}')
