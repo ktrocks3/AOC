055f6d026abdd1380c3bda268e3c764027a740f6
Kishan Thakurani <kishanthakurani2003@gmail.com>
2024-12-13 19:36:01 +0100

    Working on part 1

--- src/2024/13. Star-13/star13.py
+++ src/2024/13. Star-13/star13.py
@@ -1,3 +1,6 @@
+from scipy.optimize import linprog
+
+
 def common(filename):
     with open(filename, 'r') as f:
         lines = list(map(lambda x: x.strip(), f.readlines()))
@@ -5,7 +8,38 @@ def common(filename):
 
 
 def part1(filename):
-    return
+    c = [3, 1]
+
+    def calculate_machine(A_eq, b_eq):
+
+        # Bounds for x and y
+        bounds = [(0, 100), (0, 100)]
+
+        # Solve the linear program
+        result = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method='highs')
+
+        # Check the result
+        if result.success:
+            return round(result.fun)
+        else:
+            return 0
+
+    file = common(filename)
+    A = [[], []]
+    total = 0
+    for line in file:
+        Prize = []
+        if 'Button' in line:
+            line = [int(x.strip().split('+')[1]) for x in line.split(':')[1].strip().split(',')]
+            A[0].append(line[0])
+            A[1].append(line[1])
+        if 'Prize' in line:
+            line = line[7:].split('=')
+            Prize.append(int(line[1].strip("'").split(',')[0]))
+            Prize.append(int(line[2].strip("'")))
+            total += calculate_machine(A, Prize)
+            A = [[], []]
+    return total
 
 
 def part2(filename):
@@ -13,7 +47,7 @@ def part2(filename):
 
 
 if __name__ == '__main__':
-    assert part1('example.txt') == "", f"Received {part1('example.txt')} for example on part1"
+    assert part1('example.txt') == 480, f"Received {part1('example.txt')} for example on part1"
     # assert part1('example2.txt') == "", f"Received {part1('example2.txt')} for example2 on part1"
     print(f'Part 1 answer: {part1('input.txt')}')
     assert part2('example.txt') == "", f"Received {part2('example.txt')} for example on part2"
