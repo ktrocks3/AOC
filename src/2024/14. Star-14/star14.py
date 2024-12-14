5465f24e21e6842c84e3db2dee73f91a8d83cf9b
Kishan Thakurani <kishanthakurani2003@gmail.com>
2024-12-14 11:22:30 +0100

    Uploaded day 14 files

---
+++ src/2024/14. Star-14/star14.py
@@ -0,0 +1,21 @@
+def common(filename):
+    with open(filename, 'r') as f:
+        lines = list(map(lambda x: x.strip(), f.readlines()))
+    return lines
+
+
+def part1(filename):
+    return
+
+
+def part2(filename):
+    return
+
+
+if __name__ == '__main__':
+    assert part1('example.txt') == "", f"Received {part1('example.txt')} for example on part1"
+    # assert part1('example2.txt') == "", f"Received {part1('example2.txt')} for example2 on part1"
+    print(f'Part 1 answer: {part1('input.txt')}')
+    assert part2('example.txt') == "", f"Received {part2('example.txt')} for example on part2"
+    # assert part2('example2.txt') == "", f"Received {part2('example2.txt')} for example2 on part2"
+    print(f'Part 2 answer: {part2('input.txt')}')
