d0bb78d305adbb08cdc583d9febb6b3402328cec
Kishan Thakurani <kishanthakurani2003@gmail.com>
2024-12-15 17:06:42 +0100

    Finished part 1

--- src/2024/15. Star-15/star15.py
+++ src/2024/15. Star-15/star15.py
@@ -1,5 +1,3 @@
-from rich.console import Console
-
 def common(filename):
     with open(filename, 'r') as f:
         lines = list(map(lambda x: x.strip(), f.readlines()))
@@ -7,7 +5,62 @@ def common(filename):
 
 
 def part1(filename):
-    return
+    def print_grid():
+        for r in grid:
+            S = ""
+            for c in r:
+                if c == '@':
+                    S += "\033[31m@\033[0m"  # Red color for '@'
+                else:
+                    S += c
+            print(S)
+
+    def GPS(GPSx, GPSy):
+        return GPSx + (GPSy * 100)
+
+    file = common(filename)
+    b = True
+    grid = []
+    movement = []
+    x, y = 0, 0
+    for i, line in enumerate(file):
+        if line == '':
+            b = False
+        elif b:
+            grid.append(list(line))
+            if '@' in line:
+                x, y = line.index('@'), i
+        else:
+            movement.extend(list(line))
+    for m in movement:
+        if m == '<':
+            dy, dx = y, x - 1
+        elif m == '>':
+            dy, dx = y, x + 1
+        elif m == '^':
+            dy, dx = y - 1, x
+        elif m == 'v':
+            dy, dx = y + 1, x
+        else:
+            print(m, "BAD")
+            continue
+
+        if grid[dy][dx] == '#':
+            continue
+        elif grid[dy][dx] == '.':
+            grid[dy][dx], grid[y][x] = grid[y][x], grid[dy][dx]
+            y, x = dy, dx
+        elif grid[dy][dx] == 'O':
+            np = (dy + (dy - y), dx + (dx - x))
+            while grid[np[0]][np[1]] == 'O':
+                np = (np[0] + (dy - y), np[1] + (dx - x))
+            if grid[np[0]][np[1]] == '.':
+                grid[np[0]][np[1]], grid[dy][dx] = grid[dy][dx], grid[np[0]][np[1]]
+                grid[dy][dx], grid[y][x] = grid[y][x], grid[dy][dx]
+                y, x = dy, dx
+            elif grid[np[0]][np[1]] == '#':
+                continue
+    return sum(GPS(i,j) for i in range(len(grid[0])) for j in range(len(grid)) if grid[j][i] == 'O')
 
 
 def part2(filename):
