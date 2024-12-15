580c2f9614f8f719c0fe5f8fce2082707aef3373
Kishan Thakurani <kishanthakurani2003@gmail.com>
2024-12-15 16:35:20 +0100

    Added the day 15 examples

--- src/2024/15. Star-15/star15.py
+++ src/2024/15. Star-15/star15.py
@@ -1,3 +1,5 @@
+from rich.console import Console
+
 def common(filename):
     with open(filename, 'r') as f:
         lines = list(map(lambda x: x.strip(), f.readlines()))
@@ -13,9 +15,9 @@ def part2(filename):
 
 
 if __name__ == '__main__':
-    assert part1('example.txt') == "", f"Received {part1('example.txt')} for example on part1"
-    # assert part1('example2.txt') == "", f"Received {part1('example2.txt')} for example2 on part1"
+    assert part1('example.txt') == 2028, f"Received {part1('example.txt')} for example on part1"
+    assert part1('example2.txt') == 10092, f"Received {part1('example2.txt')} for example2 on part1"
     print(f'Part 1 answer: {part1('input.txt')}')
     assert part2('example.txt') == "", f"Received {part2('example.txt')} for example on part2"
-    # assert part2('example2.txt') == "", f"Received {part2('example2.txt')} for example2 on part2"
+    assert part2('example2.txt') == "", f"Received {part2('example2.txt')} for example2 on part2"
     print(f'Part 2 answer: {part2('input.txt')}')
