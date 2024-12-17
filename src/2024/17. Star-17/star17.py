5c35be383b62a1cc37f318c897eddd8d237e3e7c
Kishan Thakurani <kishanthakurani2003@gmail.com>
2024-12-17 16:26:45 +0100

    Finished part 1

--- src/2024/17. Star-17/star17.py
+++ src/2024/17. Star-17/star17.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def common(filename):
     with open(filename, 'r') as f:
         lines = list(map(lambda x: x.strip(), f.readlines()))
@@ -5,11 +8,61 @@ def common(filename):
 
 
 def part1(filename):
-    return
+    def combo(combo_code):
+        if combo_code <= 3:
+            return combo_code
+        if combo_code == 4:
+            return a
+        if combo_code == 5:
+            return b
+        if combo_code == 6:
+            return c
+        raise ValueError(f'{combo_code} should not appear')
+
+    file = common(filename)
+    a, b, c, prog, out = 0, 0, 0, [], []
+    for line in file:
+        if 'Register A' in line:
+            a = int(line[11:])
+        if 'Register B' in line:
+            b = int(line[11:])
+        if 'Register C' in line:
+            c = int(line[11:])
+        if 'Program' in line:
+            prog = [int(x) for x in line[8:].strip().split(',')]
+
+    i = 0
+    while i < len(prog) - 1:
+        instr, code = prog[i], prog[i + 1]
+        match instr:
+            case 0:
+                a = a // (2 ** combo(code))
+            case 1:
+                b = b ^ code
+            case 2:
+                b = combo(code) % 8
+            case 3:
+                if a == 0:
+                    i += 2
+                else:
+                    i = code
+                continue
+            case 4:
+                b = b ^ c
+            case 5:
+                out.append(combo(code) % 8)
+            case 6:
+                b = a // (2 ** combo(code))
+            case 7:
+                c = a // (2 ** combo(code))
+        i += 2
+
+    return (str(out)[1:-1]).replace(' ', ''), a, b, c
 
 
 def part2(filename):
     return
+
 
 
 if __name__ == '__main__':
@@ -21,7 +74,7 @@ if __name__ == '__main__':
     assert part1('example4.txt')[2] == 26, f"Received {part1('example4.txt')} for example2 on part1"
     assert part1('example5.txt')[2] == 44354, f"Received {part1('example5.txt')} for example2 on part1"
     assert part1('example6.txt')[0] == "4,6,3,5,6,3,5,2,1,0", f"Received {part1('example6.txt')} for example2 on part1"
-    print(f'Part 1 answer: {part1('input.txt')}')
-    assert part2('example.txt') == "", f"Received {part2('example.txt')} for example on part2"
-    assert part2('example2.txt') == "", f"Received {part2('example2.txt')} for example2 on part2"
+    print(f'Part 1 answer: {part1('input.txt')[0]}')
+    assert part1('example7.txt')[0] == "0,3,5,4,3,0", f"Received {part1('example7.txt')} for example on part2"
+    assert part2('input.txt') > 16300095239778
     print(f'Part 2 answer: {part2('input.txt')}')
